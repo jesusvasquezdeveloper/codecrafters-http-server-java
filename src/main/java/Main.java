daa457081335f0f2d3a8bb9368781a00bffd3f20
Jesus Vasquez <jesusvasquezr1998@gmail.com>
2024-07-15 23:20:29 +0200

    Add Not found response when request is not pointed to /

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -11,15 +14,26 @@ public class Main {
 
      try {
        ServerSocket serverSocket = new ServerSocket(4221);
-       Socket clientSocket = new Socket();
+       Socket clientSocket;
 
        // Since the tester restarts your program quite often, setting SO_REUSEADDR
        // ensures that we don't run into 'Address already in use' errors
        serverSocket.setReuseAddress(true);
 
        clientSocket = serverSocket.accept(); // Wait for connection from client.
+
        System.out.println("accepted new connection");
-       clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+       InputStream input = clientSocket.getInputStream();
+       BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+       String line = reader.readLine();
+       System.out.println(line);
+       String[] httpRequest = line.split(" ", 0);
+
+       if (httpRequest[1].equals("/")) {
+           clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+       } else {
+           clientSocket.getOutputStream().write("HTTP/1.1 404 Not Found\\r\\n\\r\\n".getBytes());
+       }
 
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
